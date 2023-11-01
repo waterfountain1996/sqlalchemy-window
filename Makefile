9d38194fca691e5c0c3aa4afe9170b3eeda70d3b
waterfountain1996 <roman@waterfountain.one>
2023-11-01 16:39:00 +0200

    style: add ruff linter&formatter

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
+sources := src tests
+
 test:
 	coverage run
 
 coverage:
 	coverage report
+
+typecheck:
+	mypy $(sources)
+
+lint:
+	ruff check $(sources)
+
+format:
+	ruff --fix $(sources)
--- src/sqlalchemy_window/__init__.py
+++ src/sqlalchemy_window/__init__.py
@@ -1,12 +1,25 @@
-from ._select import Select as Select
-from ._select import select as select
-from ._window import CURRENT_ROW as CURRENT_ROW
-from ._window import GROUP as GROUP
-from ._window import NO_OTHERS as NO_OTHERS
-from ._window import TIES as TIES
-from ._window import OverWindow as OverWindow
-from ._window import Window as Window
-from ._window import over_window as over_window
-from ._window import window as window
+from ._select import Select
+from ._select import select
+from ._window import CURRENT_ROW
+from ._window import GROUP
+from ._window import NO_OTHERS
+from ._window import TIES
+from ._window import OverWindow
+from ._window import Window
+from ._window import over_window
+from ._window import window
 
 __version__ = "0.1.4"
+
+__all__ = (
+    "CURRENT_ROW",
+    "GROUP",
+    "NO_OTHERS",
+    "over_window",
+    "OverWindow",
+    "select",
+    "Select",
+    "TIES",
+    "window",
+    "Window",
+)
